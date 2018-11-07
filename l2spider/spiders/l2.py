5e260d2df1ea1855913f640a9d2d2b5c35708d33
ferrum848 <alex84_84_84@mail.ru>
2018-11-07 18:40:44 +0300

    my first scrapy

---
+++ l2spider/spiders/l2.py
@@ -0,0 +1,43 @@
+import scrapy
+from l2spider.items import L2SpiderItem
+class BrickSetSpider(scrapy.Spider):
+	name = "l2spider"
+	start_urls = ['https://kanobu.ru/games/lineage-ii/screenshots/']
+	def parse(self, response):
+		NAME_SELECTOR = 'h1 ::text'
+		EML_SELECTOR = 'div.gameContent a'
+
+		test = 'a ::attr(rel)'
+		yield {'my_test': response.css(test).extract_first()}
+		
+		next_page = 'div.holder li:nth-of-type(4) a ::attr(href)'
+		yield {'next_page': response.css(next_page).extract_first()}
+		page = 'https://kanobu.ru' + response.css(next_page).extract_first()
+
+		yield scrapy.Request(page, self.parse_next)
+
+		#next = 'div.holder a'
+		#for j in response.css(next):
+		#	yield {'nexxxxxxxt': j.css('a ::attr(href)').extract_first()}
+
+		text = response.css(NAME_SELECTOR).extract_first()
+		yield {'l2': text}
+
+		for i in response.css(EML_SELECTOR):
+			TEST = 'a ::attr(href)'
+			link = i.css(TEST).extract_first()
+
+			yield L2SpiderItem(file_urls=[link])
+
+	def parse_next(self, response):
+		for k in response.css('div.news-block img'):
+			link = k.css('img ::attr(src)').extract_first()
+			yield {'next_imgs': link}
+
+		#SELECTOR = 'div.news-block img ::attr(src)'
+		#link = response.css(SELECTOR).extract_first()
+		#yield {'next_page_link': link}
+			yield L2SpiderItem(file_urls=[link])
+
+
+
